513713423e17032f42fc08e72dfbdac3465fd2c1
claudio <claudio19_89@hotmail.com>
2016-11-08 01:52:04 -0300

    Launcher initial commit

---
+++ app/src/main/java/com/launcher/claudio/launcher/GridHomeDetail.java
@@ -0,0 +1,9 @@
+package com.launcher.claudio.launcher;
+
+import android.graphics.drawable.Drawable;
+
+public class GridHomeDetail {
+    CharSequence label;
+    CharSequence name;
+    Drawable icon;
+}
---
+++ app/src/main/java/com/launcher/claudio/launcher/home_view.java
@@ -0,0 +1,74 @@
+package com.launcher.claudio.launcher;
+
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class home_view extends AppCompatActivity {
+    private PackageManager manager;
+    private List<GridHomeDetail> apps;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home);
+        loadApps();
+        loadGridView();
+    }
+
+
+    private void loadApps(){
+        manager = getPackageManager();
+        apps = new ArrayList<GridHomeDetail>();
+
+        Intent i = new Intent(Intent.ACTION_MAIN, null);
+        i.addCategory(Intent.CATEGORY_LAUNCHER);
+
+        List<ResolveInfo> availableActivities = manager.queryIntentActivities(i, 0);
+        for(ResolveInfo ri:availableActivities){
+            GridHomeDetail app = new GridHomeDetail();
+            app.label = ri.loadLabel(manager);
+            app.name = ri.activityInfo.packageName;
+            app.icon = ri.activityInfo.loadIcon(manager);
+            apps.add(app);
+        }
+    }
+
+    private GridView grid;
+    private void loadGridView(){
+        grid = (GridView)findViewById(R.id.apps_grid);
+
+        ArrayAdapter<GridHomeDetail> adapter = new ArrayAdapter<GridHomeDetail>(this, R.layout.grid_home_item, apps) {
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                if(convertView == null){
+                    convertView = getLayoutInflater().inflate(R.layout.grid_home_item, null);
+                }
+
+                ImageView appIcon = (ImageView)convertView.findViewById(R.id.item_app_icon);
+                appIcon.setImageDrawable(apps.get(position).icon);
+
+                TextView appLabel = (TextView)convertView.findViewById(R.id.item_app_label);
+                appLabel.setText(apps.get(position).label);
+
+                TextView appName = (TextView)convertView.findViewById(R.id.item_app_name);
+                appName.setText(apps.get(position).name);
+
+                return convertView;
+            }
+        };
+
+        grid.setAdapter(adapter);
+    }
+}
